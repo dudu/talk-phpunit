e9a96c3eaf9f331f7f43d6a874a20c60202b3d97
Eduardo Nascimento <eduardo@eduardonascimento.com.br>
2017-02-02 19:53:24 -0200

    Refactor tests

--- tests/unit/CalculatorTest.php
+++ tests/unit/CalculatorTest.php
@@ -6,11 +6,24 @@ use MyApp\Example\Calculator;
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSumValues()
+    public function valuesProvider()
+    {
+        return [
+            [1, 2, 3],
+            [2, 2, 4],
+            [10, 20, 30],
+            [13, 37, 50],
+        ];
+    }
+
+    /**
+     * @dataProvider valuesProvider
+     */
+    public function testSumValues($valueA, $valueB, $total)
     {
         $calculator = new Calculator;
-        $sum = $calculator->sum(1,2);
+        $sum = $calculator->sum($valueA, $valueB);
 
-        $this->assertEquals(3, $sum);
+        $this->assertEquals($total, $sum);
     }
 }
